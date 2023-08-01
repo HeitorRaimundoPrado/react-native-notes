c79e41c8c9ea8963b2c202ca0fc46ddbf6bb6a95
Heitor Prado <heitorrdpp@gmail.com>
2023-08-01 00:20:24 -0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, ScrollView, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState('');
+
+  const handleAddNewNote = () => {
+    setNotes([...notes, newNote]);
+    setNewNote('');
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={appStyles.container}>
+      <ScrollView style={notesListStyle.style}>
+        {notes.map((note, idx) => {
+          return (
+            <View style={taskContainerStyles.container} key={idx}>
+              <Text>{note}</Text>
+            </View>
+          )
+        })}
+        <View style={taskContainerStyles.container}>
+          <TextInput onChangeText={setNewNote} value={newNote}/>
+        </View>
+
+        <Button onPress={handleAddNewNote} style={taskContainerStyles.container} title="+ Add New Note">
+        </Button>
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const notesListStyle = StyleSheet.create({
+  style: {
+    display: "flex",
+    flex: 1,
+    width: 250,
+    marginTop: 50
+  }
+})
+
+const appStyles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: "column",
+    flex: 1,
+    backgroundColor: '#698dc7',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
+
+const taskContainerStyles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    color: '#000',
     alignItems: 'center',
+    padding: 10,
+    marginBottom: 5,
     justifyContent: 'center',
   },
 });
