045969869f1f70843588420c8567c4670903ce50
Heitor Prado <heitorrdpp@gmail.com>
2023-08-01 18:20:26 -0300

    feat: add option to remove task

--- App.js
+++ App.js
@@ -1,15 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, ScrollView, TextInput, Button } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, ScrollView, TextInput, Button, AppState } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState('');
 
+  const handleAppStateChange = async (nextAppState) => {
+    if (nextAppState === "background" || nextAppState === 'inactive') {
+      try {
+        await AsyncStorage.setItem('data', JSON.stringify(notes));
+      } 
+
+      catch {
+        console.log("Error saving data to AsyncStorage", error);
+      }
+    }
+  }
+
+  useEffect(() => {
+    AppState.addEventListener('change', handleAppStateChange)
+  }, [])
+
+  useEffect(() => {
+    const getDataFromAsyncStorage = async () => {
+      try {
+        const data = await AsyncStorage.getItem('data');
+        if (data !== null) {
+          setNotes(JSON.parse(data));
+        }
+
+        else {
+          console.log("No data found in async storage");
+        }
+      }
+
+      catch (error) {
+        console.log("Error while retrieving data from AsyncStorage", error);
+      }
+    }
+
+    getDataFromAsyncStorage();
+  }, [])
+
   const handleAddNewNote = () => {
     setNotes([...notes, newNote]);
     setNewNote('');
   };
+
+  const handleRemoveNote = (idx) => {
+    let notesCopy = JSON.parse(JSON.stringify(notes));
+    notesCopy.splice(idx, 1);
+    setNotes(notesCopy);
+  }
 
   return (
     <View style={appStyles.container}>
@@ -17,12 +61,13 @@ export default function App() {
         {notes.map((note, idx) => {
           return (
             <View style={taskContainerStyles.container} key={idx}>
-              <Text>{note}</Text>
+              <Text style={notesStyles.style}>{note}</Text>
+              <Button onPress={() => {handleRemoveNote(idx)}} title={'remove'}/>
             </View>
           )
         })}
         <View style={taskContainerStyles.container}>
-          <TextInput onChangeText={setNewNote} value={newNote}/>
+          <TextInput style={textInputStyle.style} onChangeText={setNewNote} value={newNote} multiline={true}/>
         </View>
 
         <Button onPress={handleAddNewNote} style={taskContainerStyles.container} title="+ Add New Note">
@@ -32,6 +77,22 @@ export default function App() {
     </View>
   );
 }
+
+const textInputStyle = StyleSheet.create({
+  style: {
+    display: 'flex',
+    flex: 1
+  }
+});
+
+const notesStyles = StyleSheet.create({
+  style: {
+    display: 'flex',
+    flex: 1,
+    width: 25,
+    marginRight: 5
+  }
+});
 
 const notesListStyle = StyleSheet.create({
   style: {
@@ -56,11 +117,12 @@ const appStyles = StyleSheet.create({
 const taskContainerStyles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "row",
     backgroundColor: '#fff',
     color: '#000',
     alignItems: 'center',
     padding: 10,
     marginBottom: 5,
-    justifyContent: 'center',
+    justifyContent: 'space-between',
   },
 });
